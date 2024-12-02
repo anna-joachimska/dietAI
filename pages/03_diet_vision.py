49263b01b375a34c3d7157a2e1a77658cb4219fa
anna <ania.joachimska@gmail.com>
2024-12-02 14:35:20 +0100

    add upload a file

--- pages/03_diet_vision.py
+++ pages/03_diet_vision.py
@@ -3,15 +3,20 @@ import base64
 from ai import vision
 
 st.title("Identify your food")
-st.write("Take a picture of your food and get nutritional info about it")
+st.write("Take a picture of your food or upload a picture of it and get nutritional info about it")
 
 col1, col2 = st.columns([1,2])
 
 with col1:
     enable_camera = st.checkbox("Enable camera")
-    picture = st.camera_input("Take a picture", disabled=not enable_camera)
+    camera_picture = st.camera_input("Take a picture", disabled=not enable_camera)
+    uploaded_picture = st.file_uploader("Upload a picture")
+
 
 with col2:
-    if picture:
-        image_data = base64.b64encode(picture.getvalue()).decode("utf-8")
+    if camera_picture:
+        image_data = base64.b64encode(camera_picture.getvalue()).decode("utf-8")
+        st.markdown(vision.generate_response(image_data))
+    elif uploaded_picture:
+        image_data = base64.b64encode(uploaded_picture.getvalue()).decode("utf-8")
         st.markdown(vision.generate_response(image_data))
